fc1c4c1ef2d026437b71d7de0f488d1047e122e6
testrungirl <laigbodioh@gmail.com>
2020-02-18 18:58:22 +0100

    Added a View and ActionResult for Create that adds a new Category. Add delete/edit/details icons to the list of category from db in index page

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models;
 
 namespace Spice.Areas.Admin.Controllers
 {
@@ -23,6 +24,43 @@ namespace Spice.Areas.Admin.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await db.Category.ToListAsync());
+        }
+
+        //Get- Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post - Create
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Category.Add(category);
+                await db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        //Get
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = await db.Category.FindAsync(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
         }
     }
 }
