7f7c484ef8f3fd32f42082cbaddbc6e1404b5715
testrungirl <laigbodioh@gmail.com>
2020-05-24 16:55:07 +0100

    Displayed the coupons as a carousel on the view

--- Areas/Customer/Controllers/HomeController.cs
+++ Areas/Customer/Controllers/HomeController.cs
@@ -1,30 +1,38 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Spice.Data;
 using Spice.Models;
+using Spice.Models.ViewModels;
 
 namespace Spice.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext _db)
         {
-            _logger = logger;
+            db = _db; 
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            IndexViewModel IndexVM = new IndexViewModel();
+            IndexVM.MenuItem = await db.MenuItem.Include(m => m.Category).Include(x=>x.SubCategory).ToListAsync();
+
+            IndexVM.Category = await db.Category.ToListAsync();
+            IndexVM.Coupon = await db.Coupon.Where(x=>x.isActive==true).ToListAsync();
+            return View(IndexVM);
         }
 
-        public IActionResult Privacy()
+        public IActionResult Privacy() 
         {
             return View();
         }
